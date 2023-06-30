dd26789a1412edbacc021e5fb44037f6c4441d1e
yunasim <ynsim@solidit.co.kr>
2023-06-30 22:53:18 +0900

    #✨feature: State Pattern 구현 완료

---
+++ StatePattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+
+
+        }
+    }
+}
---
+++ StatePattern/StatePattern.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    class Context
+    {
+        private State m_State = null;
+
+        public Context(State state)
+        {
+            this.TransitionTo(state);
+        }
+
+        // 상태 객체를 변경
+        public void TransitionTo(State state)
+        {
+            Console.WriteLine($"Context: Transition to {state.GetType().Name}");
+            this.m_State = state;
+            this.m_State.SetContext(this);
+        }
+
+        public void Request1()
+        {
+            this.m_State.Handle1();
+        }
+
+        public void Request2()
+        {
+            this.m_State.Handle2();
+        }
+    }
+
+    abstract class State
+    {
+        protected Context m_Context;
+
+        public void SetContext(Context context)
+        {
+            m_Context = context;
+        }
+
+        public abstract void Handle1();
+
+        public abstract void Handle2();
+    }
+
+    class ConcreteStateA : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateA Handle1 request");
+            m_Context.TransitionTo(new ConcreteStateB());
+        }
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateA Handle2 request");
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateB Handle1 request");
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateB Handle2 request");
+            m_Context.TransitionTo(new ConcreteStateA());
+        }
+    }
+}
