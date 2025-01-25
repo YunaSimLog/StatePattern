b84ccf078f0d1f6e5df747fbed7af50c6b502c05
yunasim <ynsim@solidit.co.kr>
2025-01-25 23:39:55 +0900

    #😎update: 상태 패턴 다른식으로 구현

---
+++ StatePatter_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+
+            // 1. StateA 상태 설정
+            context.SetState(new ConcreteStateA());
+
+            // 2. 현재 StateA 상태에 맞는 메소드 실행
+            context.Request();
+
+            // 3. StateB 상태 설정
+            context.SetState(new ConcreteStateB());
+
+            // 4. StateB 상태에 맞는 메소드 실행 후, StateC로 상태 변경
+            context.Request();
+
+            // 5. StateC 상태에 맞는 메소드 실행
+            context.Request();
+        }
+    }
+}
---
+++ StatePatter_2/StateClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern_2
+{
+    public abstract class AbstractState
+    {
+        public abstract void RequestHandle(Context cxt);
+    }
+
+    public class ConcreteStateA : AbstractState
+    {
+        public override void RequestHandle(Context cxt)
+        {
+            Console.WriteLine("ConcreteStateA - RequestHandle 실행");
+        }
+    }
+
+    public class ConcreteStateB : AbstractState
+    {
+        // 상태에서 동작을 실행한 후 바로 다른 상태로 바꾸기도 함.
+        // 예를 들어 전원 On 상태에서 끄기 동작을 실행한 후 객체 상태를 전원 Off로 변경 하듯이
+        public override void RequestHandle(Context cxt)
+        {
+            Console.WriteLine("ConcreteStateB - RequestHandle 실행");
+
+            cxt.SetState(new ConcreteStateC());
+        }
+    }
+
+    public class ConcreteStateC : AbstractState
+    {
+        public override void RequestHandle(Context cxt)
+        {
+            Console.WriteLine("ConcreteStateC - RequestHandle 실행");
+        }
+    }
+
+    public class Context
+    {
+        // Composition (구성)
+        AbstractState _state;
+
+        public void SetState(AbstractState state)
+        {
+            _state = state;
+        }
+
+        // 상태에 의존한 처리 메소드로서 state 객체에 처리를 위함.
+        public void Request()
+        {
+            _state.RequestHandle(this);
+        }
+    }
+}
